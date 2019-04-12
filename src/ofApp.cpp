e551871d2807b01204f7995d3b8b7bf504c19a12
Sheldon <sheldon0789@gmail.com>
2019-04-11 22:02:19 -0400

    Still slightly offset in x but getting better

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -30,10 +30,10 @@ void ofApp::draw(){
     
     ofSetColor(255);
     ofFill();
-    
+    ofDrawCircle(0, 0, 10); // debug
     ofPushMatrix();
     ofTranslate(-hersheyFont.getWidth(topRow, hersheyScale) * 0.5, 0);
-
+    ofDrawCircle(0, 0, 5); // debug
     vector<ofPath> paths = hersheyFont.getPaths(topRow, hersheyScale);
     for (int i = 0; i < paths.size(); i++) {
         float timeElapsed = 0;
@@ -48,32 +48,33 @@ void ofApp::draw(){
         }
         
         ofPath letter = paths.at(i);
-        float xOffset = 0;
-        float scaleFactor = hersheyScale;
-        if (timeElapsed > 0) {
-            scaleFactor = ofMap(timeElapsed, 0, animationLength, hersheyScale, maxScaleFactor*hersheyScale, true);
-            string currLetter = topRow.substr(i, i+1);
-            ofPath scaledLetter = hersheyFont.getPaths(currLetter, scaleFactor).at(0);
+        if (timeElapsed == 0) {
+            letter.draw();
+            continue;
+        }
+        float scaleFactor = ofMap(timeElapsed, 0, animationLength, hersheyScale, maxScaleFactor*hersheyScale, true);
+        string currLetter = topRow.substr(i, i+1);
+        ofPath scaledLetter = hersheyFont.getPaths(currLetter, scaleFactor).at(0);
+        
+        string strTilNow = topRow.substr(0, i);
+        float widthTilNow = hersheyFont.getWidth(strTilNow, hersheyScale);
+        float widthOfScaledLetter = hersheyFont.getWidth(currLetter, scaleFactor);
+        cout << strTilNow << " has width: " << widthTilNow << endl;
+        
+        scaledLetter.draw(); // debug
 //            xOffset = hersheyFont.getWidth(currLetter, scaleFactor) * 0.5;
 //            cout << "xOffset for " << currLetter << " is: " << xOffset << endl;
-            letter = scaledLetter;
-            //letter.scale(scaleFactor, scaleFactor);
-        }
-        
+        letter = scaledLetter;
+        //letter.scale(scaleFactor, scaleFactor);
         
         int pointsToSamplePerLine = resampleCount / letter.getOutline().size();
         for (ofPolyline line : letter.getOutline()) {
             for (glm::vec3 point : line.getResampledByCount(pointsToSamplePerLine)) {
                 float yDisplacement = maxVerticalDisplacement*sin(ofMap(timeElapsed, 0, animationLength, 0, PI));
-                
-                if (timeElapsed > 0) {
-                    ofPushMatrix();
-                    ofTranslate(0, 0, ofRandom(0, 30));
-                }
-                ofDrawCircle(point.x - xOffset, point.y - yDisplacement, dotRadius);
-                if (timeElapsed > 0) {
-                    ofPopMatrix();
-                }
+//                ofPushMatrix();
+//                ofTranslate(0, 0, ofRandom(0, 30));
+                ofDrawCircle(point.x + widthTilNow, point.y - yDisplacement, dotRadius);
+//                ofPopMatrix();
             }
         }
     }
